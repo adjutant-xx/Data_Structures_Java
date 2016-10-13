49eb575c222073f49dc140e028d88e2ef045d446
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-10-12 20:40:01 -0700

    Added a deleteVertex() method to DirectedGraph; not yet tested.

--- src/Tree/DirectedGraph.java
+++ src/Tree/DirectedGraph.java
@@ -29,13 +29,31 @@ public class DirectedGraph<T> {
         graph = addEdge(p, c, graph);
     }
 
-    public HashMap<Node<T>, Node<T>> addEdge(Node<T> parent, Node<T> child, HashMap<Node<T>,Node<T>> graph){
+    private HashMap<Node<T>, Node<T>> addEdge(Node<T> parent, Node<T> child, HashMap<Node<T>,Node<T>> graph){
         if(graph.containsKey(parent)){ // Check to see if graph already contains the parent node
             Node<T> parentObject = graph.get(parent);
             if(!parentObject.children.contains(child)){ // if directed graph does not already contain an edge from parent to child, create one:
                 parentObject.children.add(child);
                 graph.put(graph.get(parent), parentObject);
             }
+        }
+        return graph;
+    }
+
+    public void deleteVertex(Node<T> vertex){
+        graph = deleteVertex(vertex, graph);
+    }
+
+    private HashMap<Node<T>,Node<T>> deleteVertex(Node<T> vertex, HashMap<Node<T>,Node<T>> graph){
+        if(graph.containsKey(vertex)){ // if graph does contain the vertex in question, remove it's references from all edges upon removal of the vertex itself:
+            for(Node<T> key : graph.keySet()){
+                HashSet<Node<T>> mutableChildren = graph.get(key).children;
+                if(mutableChildren.remove(vertex)){
+                    key.children = mutableChildren;
+                    graph.put(key, key);
+                }
+            }
+            graph.remove(vertex);
         }
         return graph;
     }
