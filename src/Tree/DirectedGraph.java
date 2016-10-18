3bdbcdf6dc50440a996b6334931bd771d50b0f2f
Matt Paauw <matthew.e.paauw@boeing.com>
2016-10-18 08:07:55 -0700

    Removed GraphNode() class and all instances; will use a HashSet of visited nodes within graph search algorithms instead of flag variables on each individual node.

--- src/Tree/DirectedGraph.java
+++ src/Tree/DirectedGraph.java
@@ -8,36 +8,36 @@ import java.util.HashSet;
 
 public class DirectedGraph<T> {
 
-    private HashMap<T, GraphNode<T>> graph;
+    private HashMap<T, Node<T>> graph;
 
     public DirectedGraph(){
-        graph = new HashMap<T, GraphNode<T>>();
+        graph = new HashMap<T, Node<T>>();
     }
 
     public void addVertex(T value){
         graph = addVertex(value, graph);
     }
 
-    private HashMap<T, GraphNode<T>> addVertex(T value, HashMap<T, GraphNode<T>> graph){
+    private HashMap<T, Node<T>> addVertex(T value, HashMap<T, Node<T>> graph){
         if(graph.containsKey(value)){ // graph can only contain one copy of a given value
             return graph;
         }
-        GraphNode<T> newNode = new GraphNode<T>(value);
+        Node<T> newNode = new Node<T>(value);
         graph.put(value,newNode);
         return graph;
     }
 
-    public GraphNode<T> getVertex(T value){ // return the vertex corresponding to a given value:
+    public Node<T> getVertex(T value){ // return the vertex corresponding to a given value:
         return graph.get(value);
     }
 
-    public void addEdge(GraphNode<T> p, GraphNode<T> c){
+    public void addEdge(Node<T> p, Node<T> c){
         graph = addEdge(p, c, graph);
     }
 
-    private HashMap<T, GraphNode<T>> addEdge(GraphNode<T> parent, GraphNode<T> child, HashMap<T,GraphNode<T>> graph){
+    private HashMap<T, Node<T>> addEdge(Node<T> parent, Node<T> child, HashMap<T,Node<T>> graph){
         if(graph.containsValue(parent)){ // Check to see if graph already contains the parent node
-            GraphNode<T> parentObject = graph.get(parent.data);
+            Node<T> parentObject = graph.get(parent.data);
 
             if(parentObject.children == null || !parentObject.children.contains(child)){ // if directed graph does not already contain an edge from parent to child, create one:
                 parentObject.children.add(child);
@@ -47,15 +47,15 @@ public class DirectedGraph<T> {
         return graph;
     }
 
-    public void removeVertex(GraphNode<T> vertex){
+    public void removeVertex(Node<T> vertex){
         graph = removeVertex(vertex, graph);
     }
 
-    private HashMap<T,GraphNode<T>> removeVertex(GraphNode<T> vertex, HashMap<T,GraphNode<T>> graph){
+    private HashMap<T,Node<T>> removeVertex(Node<T> vertex, HashMap<T,Node<T>> graph){
         if(graph.containsKey(vertex.data)){ // if graph does contain the vertex in question, remove it's references from all edges upon removal of the vertex itself:
             for(T key : graph.keySet()){
-                GraphNode<T> value = graph.get(key);
-                HashSet<GraphNode<T>> mutableChildren = value.children;
+                Node<T> value = graph.get(key);
+                HashSet<Node<T>> mutableChildren = value.children;
                 if(mutableChildren.remove(vertex)){
                     value.children = mutableChildren;
                     graph.put(key, value);
@@ -66,13 +66,13 @@ public class DirectedGraph<T> {
         return graph;
     }
 
-    public void removeEdge(GraphNode<T> p, GraphNode<T> c){
+    public void removeEdge(Node<T> p, Node<T> c){
         graph = removeEdge(p,c, graph);
     }
 
-    private HashMap<T,GraphNode<T>> removeEdge(GraphNode<T> parent, GraphNode<T> child, HashMap<T, GraphNode<T>> graph){
+    private HashMap<T,Node<T>> removeEdge(Node<T> parent, Node<T> child, HashMap<T, Node<T>> graph){
         if(graph.containsKey(parent.data)){
-            HashSet<GraphNode<T>> mutableChildren = graph.get(parent.data).children;
+            HashSet<Node<T>> mutableChildren = graph.get(parent.data).children;
             if(mutableChildren.remove(child)){
                 parent.children = mutableChildren;
                 graph.put(parent.data, parent);
@@ -81,7 +81,7 @@ public class DirectedGraph<T> {
         return graph;
     }
 
-    public boolean isAdjacent(GraphNode<T> x, GraphNode<T> y){ // checks to see whether a single edge exists between the two nodes, in either direction
+    public boolean isAdjacent(Node<T> x, Node<T> y){ // checks to see whether a single edge exists between the two nodes, in either direction
         if(!graph.containsKey(x.data) || !graph.containsKey(y.data)){
             return false;
         }
@@ -91,12 +91,12 @@ public class DirectedGraph<T> {
         return false;
     }
 
-    public ArrayList<GraphNode<T>> getNeighbors(GraphNode<T> vertex){ // gets a list of all neighbors of a given vertex, defined as having an edge that connects the vertex to/from another neighbor
-        ArrayList<GraphNode<T>> neighbors = new ArrayList<GraphNode<T>>();
-        for(GraphNode<T> item : vertex.children){ // first, add all direct neighbors from list of target vertex's children
+    public ArrayList<Node<T>> getNeighbors(Node<T> vertex){ // gets a list of all neighbors of a given vertex, defined as having an edge that connects the vertex to/from another neighbor
+        ArrayList<Node<T>> neighbors = new ArrayList<Node<T>>();
+        for(Node<T> item : vertex.children){ // first, add all direct neighbors from list of target vertex's children
             neighbors.add(item);
         }
-        for(GraphNode<T> item : graph.values()){ // next, check all other vertices within the graph to see if they have any connection to the target vertex
+        for(Node<T> item : graph.values()){ // next, check all other vertices within the graph to see if they have any connection to the target vertex
             if(item.children.contains(vertex)){
                 neighbors.add(item);
             }
@@ -104,7 +104,7 @@ public class DirectedGraph<T> {
         return neighbors;
     }
 
-    public boolean depthFirstSearch(GraphNode<T> vertex){
+    public boolean depthFirstSearch(Node<T> vertex){
 
         return false; // lol
 
