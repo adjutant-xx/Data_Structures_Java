fb01aab861f58da86453ea2c1dd159a30e1e77f5
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-07 19:30:45 -0800

    Added a test insertion method to the LinkedList unit test class.

--- src/List/LinkedList/LinkedList_Test.java
+++ src/List/LinkedList/LinkedList_Test.java
@@ -4,4 +4,15 @@ import org.junit.Test;
 
 public class LinkedList_Test {
 
+    private LinkedList<Integer> _linkedList;
+    private int[] _listElements = {117,21,5,2,0,99999,25,55};
+
+    @Test
+    public void testInsert() throws Exception{
+        for(Integer item : _listElements){
+            boolean isInserted = _linkedList.insert(item);
+            assert(isInserted);
+        }
+    }
+
 }
