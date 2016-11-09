728f36542249cbbe7d208805fbf88d71e71e3abd
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-08 19:23:58 -0800

    Renamed unit tests within the LinkedList test file to be more reflective of the LinkedList class.

--- src/List/LinkedList/LinkedList_Test.java
+++ src/List/LinkedList/LinkedList_Test.java
@@ -8,7 +8,7 @@ public class LinkedList_Test {
     private int[] _listElements = {117,21,5,2,0,99999,25,55};
 
     @Test
-    public void testInsert() throws Exception{
+    public void testLinkedListInsert() throws Exception{
         for(Integer item : _listElements){
             boolean isInserted = _linkedList.insert(item);
             assert(isInserted);
@@ -16,30 +16,30 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testGetRoot() throws Exception{
-        testInsert();
+    public void testLinkedListGetRoot() throws Exception{
+        testLinkedListInsert();
         LinkedListNode<Integer> root = _linkedList.getRoot();
         //assert(root.getValue() == _listElements[_listElements.length - 1]);
         assert(root.getValue().equals(_listElements[_listElements.length - 1]));
     }
 
     @Test
-    public void testGetSize() throws Exception{
-        testInsert();
+    public void testLinkedListGetSize() throws Exception{
+        testLinkedListInsert();
         int size = _linkedList.getSize();
         assert(size == _listElements.length);
     }
 
     @Test
-    public void testTraverse() throws Exception{
-        testInsert();
+    public void testLinkedListTraverse() throws Exception{
+        testLinkedListInsert();
         boolean traverse = _linkedList.traverse();
         assert(traverse);
     }
 
     @Test
-    public void testContains() throws Exception{
-        testInsert();
+    public void testLinkedListContains() throws Exception{
+        testLinkedListInsert();
         int containsTrueIndex = 3, containsFalseIndex = -9999;
         boolean containsTrue = _linkedList.contains(_listElements[containsTrueIndex]);
         boolean containsFalse = _linkedList.contains(containsFalseIndex);
@@ -48,8 +48,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testFind() throws Exception{
-        testInsert();
+    public void testLinkedListFind() throws Exception{
+        testLinkedListInsert();
         int validIndex = 2, invalidIndex = -9999;
         int validIndexLocation = _linkedList.find(_listElements[validIndex]);
         int invalidIndexLocation = _linkedList.find(invalidIndex);
@@ -58,8 +58,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testInsertEnd() throws Exception{
-        testInsert();
+    public void testLinkedListInsertEnd() throws Exception{
+        testLinkedListInsert();
         int insertValue = 500;
         LinkedListNode<Integer> root = _linkedList.getRoot();
         _linkedList.insertEnd(insertValue, root);
@@ -70,8 +70,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testInsertAt() throws Exception{
-        testInsert();
+    public void testLinkedListInsertAt() throws Exception{
+        testLinkedListInsert();
         int insertValue = 1200, insertIndexLocation = 4;
         LinkedListNode<Integer> root = _linkedList.getRoot();
         _linkedList.insertAt(insertValue, insertIndexLocation, root);
@@ -82,8 +82,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testRemove() throws Exception{
-        testInsert();
+    public void testLinkedListRemove() throws Exception{
+        testLinkedListInsert();
         int validRemoveValue = 2, invalidRemoveValue = -47;
         boolean validRemoval = _linkedList.remove(validRemoveValue);
         boolean invalidRemoval = _linkedList.remove(invalidRemoveValue);
@@ -92,8 +92,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testRemoveFront() throws Exception{
-        testInsert();
+    public void testLinkedListRemoveFront() throws Exception{
+        testLinkedListInsert();
         int rootValuePreRemoval = _linkedList.getRoot().getValue(), listOldRoot = _listElements[_listElements.length - 1], listNewRoot = _listElements[_listElements.length - 2];
         assert(listOldRoot != listNewRoot);
         boolean isFrontRemoved = _linkedList.removeFront();
@@ -106,8 +106,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testRemoveEnd() throws Exception{
-        testInsert();
+    public void testLinkedListRemoveEnd() throws Exception{
+        testLinkedListInsert();
         int listOldTail = _listElements[0], listNewTail = _listElements[1], preRemovalTailIndex = _linkedList.find(listOldTail), size = _linkedList.getSize();
         assert(preRemovalTailIndex == size - 1);
         boolean containsOldTail = _linkedList.contains(listOldTail);
@@ -128,8 +128,8 @@ public class LinkedList_Test {
     }
 
     @Test
-    public void testRemoveAt() throws Exception{
-        testInsert();
+    public void testLinkedListRemoveAt() throws Exception{
+        testLinkedListInsert();
         int indexRemove = 5, indexReplace = 6, valueToRemove = _listElements[indexRemove], valueToReplace = _listElements[indexReplace], indexRemoveActual = _linkedList.find(valueToRemove), indexReplaceActual = _linkedList.find(valueToReplace);
         assert(_linkedList.contains(valueToRemove));
         assert(_linkedList.contains(valueToReplace));
