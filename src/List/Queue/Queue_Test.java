3e80adad355b99ebe24d7537a5f33ae151111513
adjutant-xx <matt.paauw@gmail.com>
2016-11-08 18:28:45 -0800

    Added boilerplate code within Queue test file.

--- src/List/Queue/Queue_Test.java
+++ src/List/Queue/Queue_Test.java
@@ -3,4 +3,8 @@ package List.Queue;
 import org.junit.Test;
 
 public class Queue_Test {
+
+    private Queue<Integer> _queue = new Queue<Integer>();
+    private int[] _queueElements = {117,21,5,2,0,99999,25,55};
+
 }
