f05cdf17ac89d2cb6df375190f717c181362a2a6
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-08 19:22:17 -0800

    Renamed unit tests within the Queue test file to be more reflective of the Queue class.

--- src/List/Queue/Queue_Test.java
+++ src/List/Queue/Queue_Test.java
@@ -8,7 +8,7 @@ public class Queue_Test {
     private int[] _queueElements = {117,21,5,2,0,99999,25,55};
 
     @Test
-    public void testEnqueue() throws Exception{
+    public void testQueueEnqueue() throws Exception{
         int insertionCount = 0;
         for(Integer item : _queueElements){
             boolean isEnqueued = _queue.enqueue(item);
@@ -19,15 +19,15 @@ public class Queue_Test {
     }
 
     @Test
-    public void testTraverse() throws Exception{
-        testEnqueue();
+    public void testQueueTraverse() throws Exception{
+        testQueueEnqueue();
         boolean isTraversed = _queue.traverse();
         assert(isTraversed);
     }
 
     @Test
-    public void testDequeue() throws Exception{
-        testEnqueue();
+    public void testQueueDequeue() throws Exception{
+        testQueueEnqueue();
         int listFrontElement = _queueElements[0];
         int dequeueValue = _queue.dequeue();
         assert(_queue.getSize() == _queueElements.length - 1);
@@ -42,8 +42,8 @@ public class Queue_Test {
     }
 
     @Test
-    public void testIsEmpty() throws Exception{
-        testEnqueue();
+    public void testQueueIsEmpty() throws Exception{
+        testQueueEnqueue();
         assert(!_queue.isEmpty());
         while(!_queue.isEmpty()){
             assert(!_queue.isEmpty());
@@ -53,8 +53,8 @@ public class Queue_Test {
     }
 
     @Test
-    public void testGetSize() throws Exception{
-        testEnqueue();
+    public void testQueueGetSize() throws Exception{
+        testQueueEnqueue();
         assert(_queue.getSize() == _queueElements.length);
         int removalCount = 0;
         while(!_queue.isEmpty()){
