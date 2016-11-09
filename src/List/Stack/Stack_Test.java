7cd6ed3d48d33814656c9ad6ea9c4a70ae0a11b2
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-08 19:55:05 -0800

    Added a unit test for the Stack push() class, tested successfully.

--- src/List/Stack/Stack_Test.java
+++ src/List/Stack/Stack_Test.java
@@ -7,4 +7,12 @@ public class Stack_Test {
     private Stack<Integer> _stack = new Stack<Integer>();
     private int[] _stackElements = {117,21,5,2,0,99999,25,55};
 
+    @Test
+    public void testStackPush() throws Exception{
+        for(Integer item : _stackElements){
+            boolean isPushed = _stack.push(item);
+            assert(isPushed);
+        }
+    }
+
 }
