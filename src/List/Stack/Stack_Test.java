b611b2724e6c71205f09265c349292404e77e889
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-08 19:36:07 -0800

    Added boilerplate code to Stack testing file.

--- src/List/Stack/Stack_Test.java
+++ src/List/Stack/Stack_Test.java
@@ -3,4 +3,8 @@ package List.Stack;
 import org.junit.Test;
 
 public class Stack_Test {
+
+    private Stack<Integer> _stack = new Stack<Integer>();
+    private int[] _stackElements = {117,21,5,2,0,99999,25,55};
+
 }
