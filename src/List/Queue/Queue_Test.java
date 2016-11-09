e799342bca293f72227a7a1804a960835599e368
adjutant-xx <matt.paauw@gmail.com>
2016-11-08 18:32:06 -0800

    Added a unit test for the Queue enqueue() method, tested successfully.

--- src/List/Queue/Queue_Test.java
+++ src/List/Queue/Queue_Test.java
@@ -7,4 +7,12 @@ public class Queue_Test {
     private Queue<Integer> _queue = new Queue<Integer>();
     private int[] _queueElements = {117,21,5,2,0,99999,25,55};
 
+    @Test
+    public void testEnqueue() throws Exception{
+        for(Integer item : _queueElements){
+            boolean isEnqueued = _queue.enqueue(item);
+            assert(isEnqueued);
+        }
+    }
+
 }
