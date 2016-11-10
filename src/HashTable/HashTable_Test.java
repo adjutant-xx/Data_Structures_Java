dc4d0f3ee37296c0abd088abbf1de2e6a87470b3
adjutant-xx <matthew.paauw@email.wsu.edu>
2016-11-09 18:54:21 -0800

    Added boilerplate code to the HashTable unit test file.

--- src/HashTable/HashTable_Test.java
+++ src/HashTable/HashTable_Test.java
@@ -3,4 +3,9 @@ package HashTable;
 import org.junit.Test;
 
 public class HashTable_Test {
+
+    private HashTable<Integer, String> _hashTable = new HashTable(8);
+    private int _hashTableKeyIterator = 0;
+    private String[] _hashTableValues = {"cat","dog","fish","mouse","bird","chicken","cow","fly"};
+
 }
