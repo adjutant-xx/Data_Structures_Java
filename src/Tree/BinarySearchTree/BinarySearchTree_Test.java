92c40a320df97b6b453de1e1c46e197c788f0942
adjutant-xx <matt.paauw@gmail.com>
2016-11-14 13:46:59 -0800

    Added a unit test for the BinarySearchTree insert() method, tested successfully.

--- src/Tree/BinarySearchTree/BinarySearchTree_Test.java
+++ src/Tree/BinarySearchTree/BinarySearchTree_Test.java
@@ -5,4 +5,12 @@ import org.junit.Test;
 public class BinarySearchTree_Test {
     private BinarySearchTree<Integer> _binarySearchTree = new BinarySearchTree<Integer>();
     private int[] _binarySearchTreeElements = {117,21,5,2,0,99999,25,55};
+
+    @Test
+    public void testBinarySearchTreeInsert() throws Exception{
+        for(Integer item : _binarySearchTreeElements){
+            boolean isInserted = _binarySearchTree.insert(item);
+            assert(isInserted);
+        }
+    }
 }
