2407a5ebd62c46b8f94e8bd0a5649544d64bb5a3
adjutant-xx <matt.paauw@gmail.com>
2016-11-14 13:41:46 -0800

    Added boilerplate code to the BST unit test file.

--- src/Tree/BinarySearchTree/BinarySearchTree_Test.java
+++ src/Tree/BinarySearchTree/BinarySearchTree_Test.java
@@ -3,4 +3,6 @@ package Tree.BinarySearchTree;
 import org.junit.Test;
 
 public class BinarySearchTree_Test {
+    private BinarySearchTree<Integer> _binarySearchTree = new BinarySearchTree<Integer>();
+    private int[] _binarySearchTreeElements = {117,21,5,2,0,99999,25,55};
 }
