64aaa4cacdd339c8e33051054dd3e2c3c8035270
Matt <matt.paauw@gmail.com>
2018-01-15 19:23:41 -0800

    Updated ListNode class with getters, setters, and builder.

--- src/list/ListNode.java
+++ src/list/ListNode.java
@@ -4,6 +4,44 @@ package list;
  * Contains implementation for a ListNode object, to be used with Linked List data structures.
  */
 public class ListNode<T> {
-    T data;
-    ListNode<T> next;
+    private T data;
+    private ListNode<T> next;
+
+    public ListNode() {}
+
+    public ListNode(Builder<T> builder) {
+        this.data = builder.data;
+        this.next = builder.next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ListNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
+
+    static class Builder<T> {
+        private T data;
+        private ListNode<T> next;
+
+        public Builder data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public Builder next(ListNode<T> next) {
+            this.next = next;
+            return this;
+        }
+    }
 }
