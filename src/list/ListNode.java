04051fb1808cf0c97a9b9e02e07fc0cd3cce1e4f
Matt <matt.paauw@gmail.com>
2018-01-15 19:02:49 -0800

    Added ListNode class.

---
+++ src/list/ListNode.java
@@ -0,0 +1,9 @@
+package list;
+
+/**
+ * Contains implementation for a ListNode object, to be used with Linked List data structures.
+ */
+public class ListNode<T> {
+    T data;
+    ListNode<T> next;
+}
