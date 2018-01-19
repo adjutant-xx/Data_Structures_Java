c0b7913293b8c479647ffcc3752d60bf8feed22a
mpaauw <matt.paauw@gmail.com>
2018-01-19 10:05:56 -0800

    Added builder class.

--- src/tree/BinaryTreeNode.java
+++ src/tree/BinaryTreeNode.java
@@ -14,6 +14,12 @@ public class BinaryTreeNode<T> extends TreeNode<T>{
         super(data);
     }
 
+    public BinaryTreeNode(BinaryTreeNodeBuilder<T> builder) {
+        this.data = builder.data;
+        this.left = builder.left;
+        this.right = builder.right;
+    }
+
     public BinaryTreeNode<T> getLeft() {
         return left;
     }
@@ -28,5 +34,27 @@ public class BinaryTreeNode<T> extends TreeNode<T>{
 
     public void setRight(BinaryTreeNode<T> right) {
         this.right = right;
+    }
+
+    public static class BinaryTreeNodeBuilder<T> {
+
+        private T data;
+        private BinaryTreeNode<T> left;
+        private BinaryTreeNode<T> right;
+
+        public BinaryTreeNodeBuilder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public BinaryTreeNodeBuilder<T> left(BinaryTreeNode<T> left) {
+            this.left = left;
+            return this;
+        }
+
+        public BinaryTreeNodeBuilder<T> right(BinaryTreeNode<T> right) {
+            this.right = right;
+            return this;
+        }
     }
 }
