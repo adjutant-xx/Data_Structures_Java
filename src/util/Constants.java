1eaf4616ab4420aa905ab1cf3b13b1dbd997f4e7
Matt <matt.paauw@gmail.com>
2018-01-22 18:09:16 -0800

    Added Constants class for globally-defined constant data.

---
+++ src/util/Constants.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * Contains globally-defined constants to be used throughout any class.
+ */
+public class Constants {
+
+    public static final String DIRECTED = "directed";
+
+    public static final String UNDIRECTED = "undirected";
+
+}
