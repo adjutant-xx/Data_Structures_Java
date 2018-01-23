f6fc0108898ec39ef2789199b8c63a34c4aa3804
Matt <matt.paauw@gmail.com>
2018-01-22 17:16:42 -0800

    Added graphnode class implementation.

---
+++ src/tree/GraphNode.java
@@ -0,0 +1,13 @@
+package tree;
+
+/**
+ * Contains implementations for a GraphNode object, to be used with Graph-based data structures.
+ * Extends the TreeNode class.
+ */
+public class GraphNode<T> extends TreeNode<T> {
+    public GraphNode() {}
+
+    public GraphNode(T data) {
+        super(data);
+    }
+}
