14d64687f3042c1528a83b41d79f70969eac96e4
Matt <matt.paauw@gmail.com>
2018-01-25 12:51:33 -0800

    Finished adding unit tests for HashTable class.

--- src/test/hashTable/HashTableTest.java
+++ src/test/hashTable/HashTableTest.java
@@ -27,9 +27,9 @@ public class HashTableTest {
     public void hashTablePutTest() throws Exception {
         String[] arr = {"John", "Jane", "Matt", "Chief", "Matt", "Foobar"};
         for(String s : arr) {
-            boolean expected1 = true;
-            boolean actual1 = this.hashTable.put(s, this.engine.generateRandomInteger());
-            Assert.assertEquals(expected1, actual1);
+            boolean expected = true;
+            boolean actual = this.hashTable.put(s, this.engine.generateRandomInteger());
+            Assert.assertEquals(expected, actual);
         }
     }
 
@@ -47,7 +47,84 @@ public class HashTableTest {
             int actual = this.hashTable.get(keyArr[i]);
             Assert.assertEquals(expected, actual);
         }
+    }
 
+    @Test
+    public void hashTableContainsKeyTest() throws Exception {
+        String[] validArr = {"Luke", "Leia", "Han", "Chewie", "C3P0", "R2", "Vader"};
+        String[] invalidArr = {"Cat", "Dog", "Bird", "Fish"};
+        for(String s : validArr) {
+            this.hashTable.put(s, this.engine.generateRandomInteger());
+        }
+        for(String s : validArr) {
+            boolean expected1 = true;
+            boolean actual1 = this.hashTable.containsKey(s);
+            Assert.assertEquals(expected1, actual1);
+        }
+        for(String s : invalidArr) {
+            boolean expected2 = false;
+            boolean actual2 = this.hashTable.containsKey(s);
+            Assert.assertEquals(expected2, actual2);
+        }
+    }
+
+    @Test
+    public void hashTableRemoveTest() throws Exception {
+        String[] arr = {"Audi", "Beamer", "Benz", "Bentley"};
+        for(String s : arr) {
+            this.hashTable.put(s, this.engine.generateRandomInteger());
+        }
+        for(String s : arr) {
+            boolean expected1 = true;
+            boolean actual1 = this.hashTable.remove(s);
+            Assert.assertEquals(expected1, actual1);
+            boolean expected2 = false;
+            boolean actual2 = this.hashTable.containsKey(s);
+            Assert.assertEquals(expected2, actual2);
+        }
+    }
+
+    @Test
+    public void hashTableGetSizeTest() throws Exception {
+        String[] arr = {"Alpha", "Beta", "Charlie", "Delta", "Echo", "Gamma", "Foxtrot"};
+        int count = 0;
+        for(String s : arr) {
+            this.hashTable.put(s, this.engine.generateRandomInteger());
+            count++;
+            int expected1 = count;
+            int actual1 = this.hashTable.getSize();
+            Assert.assertEquals(expected1, actual1);
+        }
+        for(String s : arr) {
+            this.hashTable.remove(s);
+            count--;
+            int expected2 = count;
+            int actual2 = this.hashTable.getSize();
+            Assert.assertEquals(expected2, actual2);
+        }
+    }
+
+    @Test
+    public void hashTableIsEmptyTest() throws Exception {
+        String[] arr = {"Apple", "Banana", "Cranberry", "Dumpling"};
+        boolean expected1 = true;
+        boolean actual1 = this.hashTable.isEmpty();
+        Assert.assertEquals(expected1, actual1);
+        for(String s : arr) {
+            this.hashTable.put(s, this.engine.generateRandomInteger());
+            boolean expected2 = false;
+            boolean actual2 = this.hashTable.isEmpty();
+            Assert.assertEquals(expected2, actual2);
+        }
+        for(String s : arr) {
+            boolean expected3 = false;
+            boolean actual3 = this.hashTable.isEmpty();
+            Assert.assertEquals(expected3, actual3);
+            this.hashTable.remove(s);
+        }
+        boolean expected4 = true;
+        boolean actual4 = this.hashTable.isEmpty();
+        Assert.assertEquals(expected4, actual4);
     }
 
 }
