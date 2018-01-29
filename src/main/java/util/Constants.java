b4671a2328d347ceea041fd2003fbca2c3e90a3a
Matt <matt.paauw@gmail.com>
2018-01-29 09:11:18 -0800

    Added generic error message.

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
 
     public static final String UNDIRECTED = "undirected";
 
+    public static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_INPUT_NULL = "Illegal argument encountered, '%s' cannot be null.";
+
 }
