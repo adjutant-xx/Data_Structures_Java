4ab096f8f6680bc891914395942983556a01b131
Matt <matt.paauw@gmail.com>
2018-01-30 19:54:39 -0800

    Added new constant strings.

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -11,4 +11,11 @@ public class Constants {
 
     public static final String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE_INPUT_NULL = "Illegal argument encountered, '%s' cannot be null.";
 
+    public static final String EXCEPTION_MESSAGE_STRUCTURE_FULL = "'%s' is full.";
+
+    public static final String EXCEPTION_MESSAGE_STRUCTURE_EMPTY = "'%s' is empty.";
+
+    public static final String INDEX_OUT_OF_BOUNDS_EXCEPTION_INDEX_INVALID = "Supplied index is invalid, out of bounds.";
+
+
 }
