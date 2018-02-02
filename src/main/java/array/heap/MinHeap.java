de23babb510d49705d51fd095880e5e2464da0f3
Matt <matt.paauw@gmail.com>
2018-02-01 18:06:05 -0800

    Built two heap-extending classes to reflect min/max heap structures and their slightly-different operations.

--- src/main/java/array/heap/MinHeap.java
+++ src/main/java/array/heap/MinHeap.java
@@ -12,59 +12,18 @@ public class MinHeap<T extends Comparable<T>> extends Heap<T> {
     }
 
     @Override
-    protected void upHeap() {
-        int currentIndex = this.size;
-        while(currentIndex > 0) {
-            int parentIndex = (currentIndex % 2 == 0) ? (currentIndex / 2) - 1 : currentIndex / 2;
-            if(this.heap[currentIndex].compareTo(this.heap[parentIndex]) >= 0) {
-                break;
-            }
-            swap(currentIndex, parentIndex);
-            currentIndex = parentIndex;
-        }
+    protected boolean upHeapComparator(int xIndex, int yIndex) {
+        return this.heap[xIndex].compareTo(this.heap[yIndex]) >= 0;
     }
 
     @Override
-    protected void downHeap() {
-        int currentIndex = 0;
-        while(true) {
-            int leftChildIndex = (2 * currentIndex) + 1;
-            int rightChildIndex = (2 * currentIndex) + 2;
-            if(leftChildIndex < this.size && rightChildIndex < this.size) {
-                int minIndex = findMinIndex(leftChildIndex, rightChildIndex);
-                if(compare(this.heap[currentIndex], this.heap[minIndex]) > 0) {
-                    swap(currentIndex, minIndex);
-                    currentIndex = minIndex;
-                } else {
-                    break;
-                }
-            }
-            else if(leftChildIndex < this.size) {
-                if(compare(this.heap[currentIndex], this.heap[leftChildIndex]) > 0) {
-                    swap(currentIndex, leftChildIndex);
-                    currentIndex = leftChildIndex;
-                } else {
-                    break;
-                }
-            }
-            else {
-                break;
-            }
-        }
+    protected boolean downHeapComparator(int xIndex, int yIndex) {
+        return compare(this.heap[xIndex], this.heap[yIndex]) > 0;
     }
 
-    /**
-     * Compares two values within the underlying heap array and returns the index of the minimum.
-     * @param xIndex index of first item to use in comparison.
-     * @param yIndex index of second item to use in comparison.
-     * @return integer representing index of the minimum value from the comparison.
-     * @throws IndexOutOfBoundsException
-     */
-    private int findMinIndex(int xIndex, int yIndex) throws IndexOutOfBoundsException {
-        if(xIndex >= this.size || yIndex >= this.size) {
-            throw new IndexOutOfBoundsException(Constants.INDEX_OUT_OF_BOUNDS_EXCEPTION_INDEX_INVALID);
-        }
-        return (this.heap[xIndex].compareTo(this.heap[yIndex]) <= 0) ? xIndex : yIndex;
+    @Override
+    protected boolean extremeComparator(int xIndex, int yIndex) {
+        return this.heap[xIndex].compareTo(this.heap[yIndex]) <= 0;
     }
 
 }
